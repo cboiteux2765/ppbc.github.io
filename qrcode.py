b83ad7db39a22fdade3193bd378272e7d182a939
Clement Boiteux <67805925+cboiteux2765@users.noreply.github.com>
2024-06-18 15:23:37 -0700

    Added glovebox input html form and some qr code scanning

---
+++ qrcode.py
@@ -0,0 +1,27 @@
+import qreader
+import gspread
+
+# QR code contains the data of the item
+def read_qr_code():
+    qr = qreader.QRCodeReader()
+    qr.decode('qr_code.png')
+    return qr.data
+
+def get_google_sheet():
+    gc = gspread.service_account(filename='service_account.json')
+    sheet = gc.open('Inventory')
+
+def update_sheet(data):
+    sheet = get_google_sheet()
+    # if the sheet does not contain the entry already
+    
+    sheet.values_append('Sheet1', {'values': [data]})
+
+    # else update the existing entry
+
+    sheet.values_update('Sheet1', sheet.find(data), {'values': [data]})
+
+if __name__ == '__main__':
+    get_google_sheet()
+    data = read_qr_code()
+    update_sheet(data)
